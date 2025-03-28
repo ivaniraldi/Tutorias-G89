66c700a1468461a5a431f11bdd9baebb12924e46
Ivan Iraldi <iraldiban@gmail.com>
2025-03-28 00:18:36 -0300

    React.apis

--- Modulo React.JS/REACT-APIS/frontend/src/views/Home.jsx
+++ Modulo React.JS/REACT-APIS/frontend/src/views/Home.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home({ frase }) {
   const [pizzas, setPizzas] = useState([]);
